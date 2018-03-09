6f7482602a37c3d4cfbb7d5162d27ba60a7e6655
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-03-09 15:49:40 +0100

    add a abstract_plugin_factory interface level

--- Gammou/Synthesizer/plugin_management/abstract_plugin_factory.cpp
+++ Gammou/Synthesizer/plugin_management/abstract_plugin_factory.cpp
@@ -5,12 +5,9 @@ namespace Gammou {
 
 	namespace Sound {
 
-
-
-		abstract_plugin_factory::abstract_plugin_factory(const std::string& name, const std::string& description, const unsigned int factory_id)
+		abstract_plugin_factory::abstract_plugin_factory(const std::string & name, const std::string & description, unsigned int factory_id)
 			: m_name(name), m_description(m_name), m_factory_id(factory_id)
 		{
-			
 		}
 
 		const std::string & abstract_plugin_factory::get_name() const
@@ -23,51 +20,59 @@ namespace Gammou {
 			return m_description;
 		}
 
-		const abstract_request_form & abstract_plugin_factory::get_request_form()
+		unsigned int abstract_plugin_factory::get_factory_id() const
+		{
+			return m_factory_id;
+		}
+
+		void abstract_plugin_factory::stamp_sound_component(abstract_sound_component * component) const
+		{
+			if (component == nullptr)
+				throw std::domain_error("Cannot stamp null component");
+			else if (component->m_factory_id != NO_FACTORY && component->m_factory_id != m_factory_id)
+				throw std::domain_error("Sound component is already stamped");
+			else
+				component->m_factory_id = m_factory_id;
+		}
+
+		///////////////
+
+		plugin_factory::plugin_factory(const std::string& name, const std::string& description, const unsigned int factory_id)
+			: abstract_plugin_factory(name, description, factory_id)
+		{
+			
+		}
+
+		const abstract_request_form & plugin_factory::get_request_form()
 		{
 			if (!m_plugin_request_form)
 				m_plugin_request_form = create_plugin_request_form();
 			return (*m_plugin_request_form);
 		}
 
-		abstract_sound_component * abstract_plugin_factory::get_new_sound_component(data_source & source, const unsigned int channel_count)
+		abstract_sound_component * plugin_factory::get_new_sound_component(data_source & source, const unsigned int channel_count)
 		{
 			abstract_sound_component *component = create_sound_component(source, channel_count);
 			stamp_sound_component(component);	//	set factory ID
 			return component;
 		}
 
-		abstract_sound_component * abstract_plugin_factory::get_new_sound_component(const abstract_form_answer& answer, const unsigned int channel_count)
+		abstract_sound_component * plugin_factory::get_new_sound_component(const abstract_form_answer& answer, const unsigned int channel_count)
 		{
 			abstract_sound_component *component = create_sound_component(answer, channel_count);
 			stamp_sound_component(component);
 			return component;
 		}
 
-		unsigned int abstract_plugin_factory::get_factory_id() const
-		{
-			return m_factory_id;
-		}
-
-		void abstract_plugin_factory::delete_sound_component(abstract_sound_component * component) const
+		void plugin_factory::delete_sound_component(abstract_sound_component * component) const
 		{
 			if( component != nullptr)
 				delete component;
 		}
 
-		std::unique_ptr<abstract_request_form> abstract_plugin_factory::create_plugin_request_form()
+		std::unique_ptr<abstract_request_form> plugin_factory::create_plugin_request_form()
 		{
 			return create_empty_request_form(); // Default implementation
-		}
-		
-		void abstract_plugin_factory::stamp_sound_component(abstract_sound_component * component) const
-		{
-			if(component == nullptr)
-				throw std::domain_error("Cannot stamp null component");
-			else if (component->m_factory_id != NO_FACTORY && component->m_factory_id != m_factory_id)
-				throw std::domain_error("Sound component is already stamped");
-			else
-				component->m_factory_id = m_factory_id;
 		}
 
 
--- Gammou/Synthesizer/plugin_management/abstract_plugin_factory.h
+++ Gammou/Synthesizer/plugin_management/abstract_plugin_factory.h
@@ -12,34 +12,50 @@ namespace Gammou {
 
     namespace Sound {
 
-        class abstract_plugin_factory{
+		class abstract_plugin_factory {
 
-        public:
+		public:
 			abstract_plugin_factory(const std::string& name, const std::string& description, unsigned int factory_id);
 			virtual ~abstract_plugin_factory() {}
 
 			const std::string& get_name() const;
 			const std::string& get_description() const;
-			const abstract_request_form& get_request_form();
-			
-			abstract_sound_component *get_new_sound_component(data_source& source, const unsigned int channel_count);
-			abstract_sound_component *get_new_sound_component(const abstract_form_answer& answer, const unsigned int channel_count);
-
 			unsigned int get_factory_id() const;
-			void delete_sound_component(abstract_sound_component*component) const;
+
+			virtual const abstract_request_form& get_request_form() = 0;
+			virtual abstract_sound_component *get_new_sound_component(data_source& source, const unsigned int channel_count) = 0;
+			virtual abstract_sound_component *get_new_sound_component(const abstract_form_answer& answer, const unsigned int channel_count) = 0;
+
+			virtual void delete_sound_component(abstract_sound_component*component) const = 0;
+
+		protected:
+			void stamp_sound_component(abstract_sound_component *component) const;
+
+		private:
+			const std::string m_name;
+			const std::string m_description;
+			const unsigned int m_factory_id;
+		};
+
+        class plugin_factory : public abstract_plugin_factory {
+
+        public:
+			plugin_factory(const std::string& name, const std::string& description, unsigned int factory_id);
+			virtual ~plugin_factory() {}
+			
+			const abstract_request_form& get_request_form() override;
+
+			abstract_sound_component *get_new_sound_component(data_source& source, const unsigned int channel_count) override;
+			abstract_sound_component *get_new_sound_component(const abstract_form_answer& answer, const unsigned int channel_count) override;
+
+			void delete_sound_component(abstract_sound_component*component) const override;
 
 		protected:
 			virtual std::unique_ptr<abstract_request_form> create_plugin_request_form();
 			virtual abstract_sound_component *create_sound_component(data_source& source, const unsigned int channel_count) = 0;
 			virtual abstract_sound_component *create_sound_component(const abstract_form_answer& answer_form, const unsigned int channel_count) = 0;
-
+		
 		private:
-			void stamp_sound_component(abstract_sound_component *component) const;
-
-			const std::string m_name;
-			const std::string m_description;
-			const unsigned int m_factory_id;
-
 			std::unique_ptr<abstract_request_form> m_plugin_request_form;
         };
 		
