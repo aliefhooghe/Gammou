b908adaf3582cb3727f146084ad7de5aa713ef36
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-03-24 17:39:31 +0100

    cairo_definition header correcion

--- Gammou/View/cairo_definition.h
+++ Gammou/View/cairo_definition.h
@@ -1,12 +1,13 @@
 #ifndef CAIRO_DEFINITION_H_
 #define CAIRO_DEFINITION_H_
 
-#define CAIRO_WIN32_STATIC_BUILD
-#include <cairo.h>
 
 #ifdef _WIN32
+#define CAIRO_WIN32_STATIC_BUILD
+#include <cairo.h>
 #include <cairo-win32.h>
 #elif defined __linux__
+#include <cairo.h>
 #include <cairo-xlib.h>
 #endif
 
