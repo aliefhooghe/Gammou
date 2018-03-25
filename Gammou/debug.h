e268933fcf7928046f143b2b8dc238d8ee894acb
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-03-25 15:18:22 +0200

    Use of snprintf (more secure) for Windows's DEBUG_PRINT

--- Gammou/debug.h
+++ Gammou/debug.h
@@ -6,7 +6,7 @@
 
 #ifdef _WIN32
 #include <windows.h>
-#define DEBUG_PRINT(...) {char str[256];sprintf(str, __VA_ARGS__);OutputDebugStringA(str);}
+#define DEBUG_PRINT(...) {char str[256];snprintf(str, 256, __VA_ARGS__);OutputDebugStringA(str);}
 #else
 #include <stdio.h>
 #define DEBUG_PRINT(...) std::fprintf(stderr, __VA_ARGS__)
