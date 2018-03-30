907d9688b064d8b4d1049997538ea4feec9e3ed8
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-03-31 00:20:29 +0200

    Plugin template

---
+++ Gammou/Plugins/plugins_ids.h
@@ -0,0 +1,19 @@
+#ifndef GAMMOU_PLUGINS_IDS_H_
+#define GAMMOU_PLUGINS_IDS_H_
+
+
+
+namespace Gammou {
+	namespace Sound {
+
+		enum plugins_id : unsigned int {
+			template_id = 10000,
+			plugin1_id	= 10001
+		};
+
+	}
+}
+
+
+
+#endif
