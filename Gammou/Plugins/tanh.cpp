3a112761ae7528823d42087f6a2b8678bebf9f3d
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-04-25 13:08:43 +0200

    tanh

--- Gammou/Plugins/tanh.cpp
+++ Gammou/Plugins/tanh.cpp
@@ -11,31 +11,21 @@ public:
 	~Arctan_component() {}
 
 	void process(const double input[]) override;
-	void initialize_process() override;
-
-private:
-	multi_channel_variable<double> m_frequence_integral;
 };
 
 Arctan_component::Arctan_component(const unsigned int channel_count)
-	: sound_component("Arctan", 1, 1, channel_count),
-	m_frequence_integral(this)
+	: sound_component("Arctan", 1, 1, channel_count)
 {
 }
 
 void Arctan_component::process(const double input[])
 {
-	m_output[0] = atan(input[0]);
-}
-
-void Arctan_component::initialize_process()
-{
-	m_frequence_integral = 0.0;
+	m_output[0] = tanh(input[0]);
 }
 
 EXPORT_DEFAULT_FACTORY(
 	Arctan_component, 
-	"Arctan", 
+	"Tanh", 
 	"Calculus", 
 	636363635
 )
