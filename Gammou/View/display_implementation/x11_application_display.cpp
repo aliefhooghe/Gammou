415cbe4e6b29b018d4d478b194712986ab5524ea
ArthurLiefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-06-30 22:17:04 +0200

    += x11_application_display

---
+++ Gammou/View/display_implementation/x11_application_display.cpp
@@ -0,0 +1,27 @@
+
+#include "x11_application_display.h"
+
+namespace Gammou {
+
+    namespace View {
+
+        x11_application_display::x11_application_display(
+            View::widget& root_widget)
+        :   abstract_display(root_widget),
+            abstract_x11_display(root_widget),
+            abstract_application_display(root_widget)
+        {
+        }
+
+        x11_application_display::~x11_application_display()
+        {
+        }
+
+        void x11_application_display::open(const std::string& title)
+        {
+            create_window(nullptr, title);
+        }
+
+    }   /* Gammou */
+
+}   /* Gammou */
---
+++ Gammou/View/display_implementation/x11_application_display.h
@@ -0,0 +1,26 @@
+#ifndef X11_APPLICATION_DISPLAY_H_
+#define X11_APPLICATION_DISPLAY_H_
+
+#include "display/abstract_x11_display.h"
+#include "display/abstract_application_display.h"
+
+namespace Gammou {
+
+    namespace View {
+
+        class x11_application_display : 
+            public abstract_x11_display,
+            public abstract_application_display {
+
+        public:
+            x11_application_display(View::widget& root_widget);
+            ~x11_application_display();
+
+            void open(const std::string& title) override;
+        };
+
+    }   /* Gammou */
+
+}   /* Gammou */
+
+#endif
