32afc82e4b12bee99428d7b4e55005280fa9bab3
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-03-31 17:32:11 +0200

    Add Oscilattors and filter package. add Lp1 filter.

---
+++ src/packages/filters/lp1/process.c
@@ -0,0 +1,18 @@
+
+#include <math.h>
+
+/**
+ *  \brief A first order low pass filter
+ *  \param state filter state : a floating point number
+ *  \param in one input sample
+ *  \param cutoff_freq filter cutoff frequency in Hz
+ *  \param out one filter output sample
+ */
+void node_process(void *state, float in, float cutoff_freq, float *out)
+{
+    float *filter_state = (float*)state;
+    const float dt = 1.0f / 48000.f;
+    const float g = M_PI * cutoff_freq * dt;
+    *out = (*filter_state + g * in) / (1.f + g);
+    *filter_state = *out * 2.f - *filter_state;
+}
