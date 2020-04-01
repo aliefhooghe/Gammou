d8cc6160d008e9c002cada942e0a79fdfa168a1e
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-04-01 19:00:02 +0200

    Integrate midi learn in knob widget node

--- src/gui/control_node_widgets/knob_node_widget.cpp
+++ src/gui/control_node_widgets/knob_node_widget.cpp
@@ -7,7 +7,7 @@ namespace Gammou {
     class knob_node_widget : public owning_node_widget {
         using parameter = synthesizer::parameter;
     public:
-        knob_node_widget(parameter && param)
+        knob_node_widget(synthesizer& synth, parameter && param)
         :   owning_node_widget{
                 "Knob",
                 std::make_unique<DSPJIT::reference_compile_node>(param.get_value_ref())
@@ -22,12 +22,19 @@ namespace Gammou {
             knob->set_callback([this](float val) { _param.set(val);});
             knob->set_value(*_param.get_value_ref());
 
-            //  Insert the knob widget
+            //  Create the midi learn button
+            auto button = std::make_unique<View::text_push_button>("L", 1.5f, 1.5f);
+            button->set_callback([this, &synth](){ synth.midi_learn(_param); });
+
+            //  Insert the widgets
             resize_height(node_widget::node_header_size * 2.f + knob->height());
             insert_widget(
                 (width() - knob->width()) / 2,
                 node_widget::node_header_size * 1.5f,
                 std::move(knob));
+
+            insert_widget(
+                node_widget::node_header_size, node_widget::node_header_size, std::move(button));
         }
 
     private:
@@ -43,6 +50,7 @@ namespace Gammou {
 
     std::unique_ptr<node_widget> knob_node_widget_plugin::create_node()
     {
-        return std::make_unique<knob_node_widget>(_synth.allocate_parameter());
+        return std::make_unique<knob_node_widget>(
+            _synth, _synth.allocate_parameter());
     }
 }
