6395f8be6313309b6b78c4137b27723dc01b92b4
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-04-04 23:45:58 +0200

    Use faster sinus computation

--- src/packages/oscillators/sin/process.c
+++ src/packages/oscillators/sin/process.c
@@ -1,12 +1,29 @@
 
-#include <math.h>
+#define PI 3.141592653589793f
+
+static inline float my_sin(float x)
+{
+    return x * (
+        1.f - x * x *
+        (
+            (1.f / 6.f) - x * x *
+            (
+                (1.f / 120.f) - x * x *
+                (
+                    (1.f / 5040.f)
+                    - (x * x / 362880.f)
+                )
+            )
+        )
+    );
+}
 
 void node_process(void *state, float freq, float p, float *out)
 {
     float *phase = (float*)state;
-    *out = sin(*phase + p);
+    *out = my_sin(*phase + p);
 
-    *phase += (freq / 48000.f) * 2.f * M_PI;
-    if (*phase > 2.f * M_PI + 0)
-        *phase -= (2.f * M_PI);
+    *phase += (freq / 48000.f) * PI * 2.f;
+    if (*phase > PI)
+        *phase -= PI * 2.f;
 }
