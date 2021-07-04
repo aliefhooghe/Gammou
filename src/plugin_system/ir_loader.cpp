f64aeefa18e7350ec13c8c1e7b2d476a4fe76e16
Arthur Liefhooghe <aliefhooghe@gmail.com>
2021-07-05 01:58:47 +0200

    Wav loader : verify loaded ir

--- src/plugin_system/ir_loader.cpp
+++ src/plugin_system/ir_loader.cpp
@@ -1,8 +1,13 @@
 
 #include "ir_loader.h"
+#include "log.h"
+
 #include <llvm/IRReader/IRReader.h>
+#include <llvm/IR/Verifier.h>
 #include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/raw_os_ostream.h>
 #include <llvm/Linker/Linker.h>
+#include <iostream>
 
 namespace Gammou {
 
@@ -10,10 +15,15 @@ namespace Gammou {
     {
         llvm::SMDiagnostic error;
         std::unique_ptr<llvm::Module> module{};
+        llvm::raw_os_ostream stream{std::cout};
 
         for (auto i = 0u; i < paths.size(); ++i) {
             auto loaded_module = llvm::parseIRFile(paths[i].generic_string(), error, ctx);
 
+            if (llvm::verifyModule(*loaded_module, &stream))
+                throw std::invalid_argument("IR module is broken");
+
+            // link the modules togethers
             if (i == 0u)
                 module = std::move(loaded_module);
             else
