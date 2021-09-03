351225cedc725e8cf391934b9410fb64ec47cf34
Arthur Liefhooghe <arthurliefh@orange.fr>
2021-09-03 21:33:36 +0200

    moog filter : clamp res parameter

--- src/packages/virtual_analog_filters/moog_ladder/process.c
+++ src/packages/virtual_analog_filters/moog_ladder/process.c
@@ -17,7 +17,8 @@ void node_process(struct filter_state *state, float in, float cutoff_freq, float
 {
     const float omega_c = 6.28318530718f * clamp(cutoff_freq, 1, _sample_rate / 2.f) / _sample_rate;
     const float glp1 = omega_c * (0.9892f - omega_c * (0.4342f - omega_c * (0.1381f - 0.0202f * omega_c)));
-    float x = fast_tanh(in * (1.f - 2.f * g_res) - 4.f * g_res * state->state[8]);
+    const float safe_gres = clamp(g_res, 0.f, 20.f);
+    float x = fast_tanh(in * (1.f - 2.f * safe_gres) - 4.f * safe_gres * state->state[8]);
 
     for (int i = 0; i < 4; ++i) {
         float tmp = 0.7692307692307692f * x + 0.23076923076923075f * state->state[2 * i];
