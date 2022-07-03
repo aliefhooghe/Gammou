bd848b40e2ff28dcfbef61e844dbb2c4fd76b8a8
Arthur Liefhooghe <arthurliefh@orange.fr>
2022-07-03 11:56:27 +0200

    Use math.h sin function for sin oscilator
    - The taylor approximation does not handle well the
      fast pitch variations.

--- src/packages/base/sin_oscillator/process.c
+++ src/packages/base/sin_oscillator/process.c
@@ -1,4 +1,6 @@
 
+#include <math.h>
+
 #include <synthesizer_def.h>
 #include <math_constant.h>
 #include <math_utils.h>
@@ -16,7 +18,7 @@ void node_initialize(struct sin_state *state)
 void node_process(struct sin_state *state, float freq, float *out)
 {
     const float limit = _sample_rate / 2.f;
-    *out = fast_sin(state->phase);
+    *out = sin(state->phase);
     state->phase += clamp(freq, -limit, limit) / _sample_rate * CST_2_PI;
     if (state->phase > CST_PI)
         state->phase -= CST_2_PI;
